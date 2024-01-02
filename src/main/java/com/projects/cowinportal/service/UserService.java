ee1b155d6bd479a05b093a7bd23c0e95fe8f2373
ManoharSamuel <manohar.sam.777@gmail.com>
2024-01-02 22:06:56 +0530

    Updated User service code files

--- src/main/java/com/projects/cowinportal/service/UserService.java
+++ src/main/java/com/projects/cowinportal/service/UserService.java
@@ -5,10 +5,14 @@ import com.projects.cowinportal.dto.BookAppointmentResponseDTO;
 import com.projects.cowinportal.models.Appointment;
 import com.projects.cowinportal.models.TimeSlot;
 import com.projects.cowinportal.models.User;
+import com.projects.cowinportal.models.Vaccine;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService {
+    public List<Vaccine> search();
     public Appointment bookAppointment(User user, TimeSlot timeSlot);
     public boolean cancelAppointment(long referenceNumber);
 }
--- src/main/java/com/projects/cowinportal/service/UserServiceImpl.java
+++ src/main/java/com/projects/cowinportal/service/UserServiceImpl.java
@@ -5,8 +5,16 @@ import com.projects.cowinportal.dto.BookAppointmentResponseDTO;
 import com.projects.cowinportal.models.Appointment;
 import com.projects.cowinportal.models.TimeSlot;
 import com.projects.cowinportal.models.User;
+import com.projects.cowinportal.models.Vaccine;
+
+import java.util.List;
 
 public class UserServiceImpl implements UserService{
+
+    @Override
+    public List<Vaccine> search() {
+        return null;
+    }
 
     @Override
     public Appointment bookAppointment(User user, TimeSlot timeSlot) {
