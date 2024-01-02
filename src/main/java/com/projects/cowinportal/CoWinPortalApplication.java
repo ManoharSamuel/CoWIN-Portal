8e81d62046787ddf23afa044190715eb01ef10bd
ManoharSamuel <manohar.sam.777@gmail.com>
2024-01-02 16:14:04 +0530

    Added the initial project setup files

---
+++ src/main/java/com/projects/cowinportal/CoWinPortalApplication.java
@@ -0,0 +1,13 @@
+package com.projects.cowinportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWinPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoWinPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/cowinportal/CoWinPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.cowinportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWinPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
